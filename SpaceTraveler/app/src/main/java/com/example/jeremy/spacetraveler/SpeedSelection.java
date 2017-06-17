ec02354060a628cf66d17c76a269615c7b4994c9
Jeremy T <thorjere038@granderie.ca>
2017-06-17 18:08:24 -0400

    added project

---
+++ SpaceTraveler/app/src/main/java/com/example/jeremy/spacetraveler/SpeedSelection.java
@@ -0,0 +1,13 @@
+package com.example.jeremy.spacetraveler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeedSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speed_selection);
+    }
+}
