6ec05cfe31e0d57ad083c631880b60823ac35dda
Jeremy T <thorjere038@granderie.ca>
2017-06-17 20:40:12 -0400

    added continue button funct and background image

---
+++ Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/DestinationSelectorEmpty.java
@@ -0,0 +1,15 @@
+package com.example.jeremy.earthandspaceapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DestinationSelectorEmpty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination_selector_empty);
+    }
+}
--- Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/speedSelection.java
+++ Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/speedSelection.java
@@ -1,7 +1,10 @@
 package com.example.jeremy.earthandspaceapp2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class speedSelection extends AppCompatActivity {
@@ -10,5 +13,19 @@ public class speedSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed_selection);
+
+        configurecontinueButton();
     }
+
+    private void configurecontinueButton(){
+        Button continueButton = (Button) findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(speedSelection.this, DestinationSelectorEmpty.class));
+            }
+        });
+    }
+
+
 }
