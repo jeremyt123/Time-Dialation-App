64890a9b6444e2156a628a345b98a4b2ad24145b
Jeremy T <thorjere038@granderie.ca>
2017-06-19 13:42:14 -0400

    picture formatting

--- Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/DestinationSelectorEmpty.java
+++ Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/DestinationSelectorEmpty.java
@@ -1,5 +1,6 @@
 package com.example.jeremy.earthandspaceapp2;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,5 +12,22 @@ public class DestinationSelectorEmpty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination_selector_empty);
+    }
+    //formatting large image
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight){
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if(height > reqHeight || width > reqWidth){
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            while((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth ){
+                inSampleSize *= 2;
+
+            }
+        }
+        return inSampleSize;
     }
 }
