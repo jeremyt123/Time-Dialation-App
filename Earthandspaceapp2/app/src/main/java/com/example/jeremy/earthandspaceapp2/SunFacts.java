53238cd41856242804e986aeb3874d17a6f9b771
Jeremy T <thorjere038@granderie.ca>
2017-06-20 18:48:04 -0400

    added travel time calc for earth

--- Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/SunFacts.java
+++ Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/SunFacts.java
@@ -6,13 +6,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class SunFacts extends AppCompatActivity {
+
+    TextView sunTFECalc;
+    TextView sunTFSCalc;
+    BigDecimal one = new BigDecimal("1");
+    BigDecimal secInYear = new BigDecimal("31536000");
+    BigDecimal sunDistance = new BigDecimal("150000000000");
+    double speedLight = 299792458;
+    double speedInput = speedSelection.speedInput;
+    BigDecimal speed = BigDecimal.valueOf(speedInput);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Calculating time for ship
         setContentView(R.layout.activity_sun_facts);
+
+        sunTFSCalc = (TextView) findViewById(R.id.sunTFSCalc);
+
+        BigDecimal sunTFSYear = sunDistance.divide(speed, 5, 5).divide(secInYear, 5, 5);
+        sunTFSCalc.setText(sunTFSYear.toPlainString() + " Years");
+
+        //Calculating time for earth
+        sunTFECalc = (TextView) findViewById(R.id.sunTFECalc);
+
+        //converting the time for earth to double to ease calculation
+        double sunTFSYearDouble = sunTFSYear.doubleValue();
+        //calculating time dialation
+        double sunTFEYear = sunTFSYearDouble / (1 - Math.sqrt((Math.pow(speedInput, 2) / Math.pow(speedLight, 2))));
+
+
+        sunTFECalc.setText(sunTFEYear + " Years");
     }
 
 }
--- Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/speedSelection.java
+++ Earthandspaceapp2/app/src/main/java/com/example/jeremy/earthandspaceapp2/speedSelection.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigInteger;
 
 public class speedSelection extends AppCompatActivity {
+
+    public EditText speedTxt;
+    public static int speedInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +21,19 @@ public class speedSelection extends AppCompatActivity {
         setContentView(R.layout.activity_speed_selection);
 
         configurecontinueButton();
+        speedTxt = (EditText) findViewById(R.id.userSpeed);
     }
-
+    //checking for button press
     private void configurecontinueButton(){
         Button continueButton = (Button) findViewById(R.id.continueButton);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //getting the value from the speed field
+                speedInput = Integer.parseInt(speedTxt.getText().toString());
+                //turning the activity to the destination selector
                 startActivity(new Intent(speedSelection.this, Destinationselector.class));
             }
         });
     }
-
-
 }
